ace5ea8de7a368c043f80d7916224b5504550b09
Антон Вальченко <aavalchenko@edu.hse.ru>
2018-06-12 01:39:44 +0500

    Задание решено

---
+++ Practice__5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using InputOutputLib;
+
+namespace Practice__5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Значение заданного выражения равно : {0}.", GetAnswer(GetMatrix()));
+            Console.ReadKey();
+        }
+
+        private static Double[][] GetMatrix()
+        {
+            Int32 n = Get.Int32("Введите размер действительной квадратной матрицы: ", 0);
+            Double[][] matrix = new Double[n][];
+            for (int i = 0; i < n; i++)
+            {
+                matrix[i] = new Double[n];
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i][j] = Get.Double("Введите значение в ячейке [" + i + "," + j + "]: ");
+                }
+            }
+            return matrix;
+        }
+        private static Double GetAnswer(Double[][] matrix)
+        {
+            Double output = 0;
+            Int32 count = matrix.Rank;
+            for (int i = 0; i < matrix.Rank; i++)
+            {
+                output += matrix[i].Max() * matrix[count].Max();
+                count--;
+            }
+
+            return output;
+        }
+    }
+}
